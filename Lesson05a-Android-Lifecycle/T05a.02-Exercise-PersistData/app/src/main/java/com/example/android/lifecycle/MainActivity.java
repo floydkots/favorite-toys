514de4a8455b8f22d415f1e5f04f9c6b055d314f
Floyd Kots <floydkotohoyoh@gmail.com>
2018-06-11 10:07:30 +0300

    Persist state in between app configuration changes

--- Lesson05a-Android-Lifecycle/T05a.02-Exercise-PersistData/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.02-Exercise-PersistData/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    // TODO (1) Create a key String called LIFECYCLE_CALLBACKS_TEXT_KEY
+    // COMPLETED TODO (1) Create a key String called LIFECYCLE_CALLBACKS_TEXT_KEY
+    private static final String LIFECYCLE_CALLBACKS_TEXT_KEY = "lifecycleCallbacksTextKey";
 
     /* Constant values for the names of each respective lifecycle callback */
     private static final String ON_CREATE = "onCreate";
@@ -49,8 +50,12 @@ public class MainActivity extends AppCompatActivity {
 
         mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
 
-        // TODO (6) If savedInstanceState is not null and contains LIFECYCLE_CALLBACKS_TEXT_KEY, set that text on our TextView
-
+        // COMPLETED TODO (6) If savedInstanceState is not null and contains LIFECYCLE_CALLBACKS_TEXT_KEY, set that text on our TextView
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(LIFECYCLE_CALLBACKS_TEXT_KEY)) {
+                mLifecycleDisplay.setText(savedInstanceState.getCharSequence(LIFECYCLE_CALLBACKS_TEXT_KEY));
+            }
+        }
         logAndAppend(ON_CREATE);
     }
 
@@ -137,11 +142,20 @@ public class MainActivity extends AppCompatActivity {
         logAndAppend(ON_DESTROY);
     }
 
-    // TODO (2) Override onSaveInstanceState
-    // Do steps 3 - 5 within onSaveInstanceState
-    // TODO (3) Call super.onSaveInstanceState
-    // TODO (4) Call logAndAppend with the ON_SAVE_INSTANCE_STATE String
-    // TODO (5) Put the text from the TextView in the outState bundle
+    // COMPLETED TODO (2) Override onSaveInstanceState
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Do steps 3 - 5 within onSaveInstanceState
+        // COMPLETED TODO (3) Call super.onSaveInstanceState
+        // COMPLETED TODO (4) Call logAndAppend with the ON_SAVE_INSTANCE_STATE String
+        // COMPLETED TODO (5) Put the text from the TextView in the outState bundle
+        super.onSaveInstanceState(outState);
+        logAndAppend(ON_SAVE_INSTANCE_STATE);
+        String textContents = mLifecycleDisplay.getText().toString();
+        outState.putCharSequence(LIFECYCLE_CALLBACKS_TEXT_KEY, textContents);
+    }
+
 
     /**
      * Logs to the console and appends the lifecycle method name to the TextView so that you can
--- Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -39,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    // COMPLETED TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    private static ArrayList<String> mLifeCycleCallbacks = new ArrayList<>();
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -71,9 +75,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        // COMPLETED TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        for (int j = mLifeCycleCallbacks.size() - 1; j >= 0; j--) {
+            mLifecycleDisplay.append(mLifeCycleCallbacks.get(j) + "\n");
+        }
 
-        // TODO (5) Clear mLifecycleCallbacks after iterating through it
+
+        // COMPLETED TODO (5) Clear mLifecycleCallbacks after iterating through it
+        mLifeCycleCallbacks.clear();
 
         logAndAppend(ON_CREATE);
     }
@@ -133,7 +142,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        // COMPLETED TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        mLifeCycleCallbacks.add(ON_STOP);
 
         logAndAppend(ON_STOP);
     }
@@ -160,7 +170,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        // COMPLETED TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        mLifeCycleCallbacks.add(ON_DESTROY);
 
         logAndAppend(ON_DESTROY);
     }
